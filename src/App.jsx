35e344a6cb4a196eabd27c04d2b8c6f1222e0684
김두홍 <dhkim@cyglobal.net>
2025-12-04 14:36:10 +0900

    ADD: 프로젝트 초기 구조 설정

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <>      
+      <header className="App-header">
+        <h1>Fillog</h1>
+        <p>Fill(筆) the blog with your own story</p>
+      </header>
+    </>
+  )
+}
+
+export default App
+
